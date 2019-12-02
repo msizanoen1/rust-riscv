418ba43391987ccc524cbfaa336ae08fe189559c
msizanoen1 <qtmlabs@protonmail.com>
2019-12-02 12:47:00 +0700

    Cargo default lib metadata

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 rm -rf target/riscv64gc-unknown-linux-gnu
 export RUSTFLAGS="--remap-path-prefix=$PWD=/rustc --remap-path-prefix=$HOME/.cargo=/cargo"
 export CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_GNU_LINKER=riscv64-unknown-linux-gnu-gcc
+export __CARGO_DEFAULT_LIB_METADATA=nightly
 export CC_riscv64gc_unknown_linux_gnu=riscv64-unknown-linux-gnu-gcc
 export CFLAGS_riscv64gc_unknown_linux_gnu="-mabi=lp64d -fPIC"
 cargo build \
